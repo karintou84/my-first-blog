702223112609777fc7c9bdc8cae6377314b48866
kanon <g17944sk@gm.tsuda.ac.jp>
2020-06-09 23:10:36 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/karintou84/djangogirls/blog/staic'
+STATIC_ROOT = os.path.join(BASE_DIR, 'deploy')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
